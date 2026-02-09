abcf5901f2f8c852cecc1e0420f163a5ff84d55e
Neha Vaghela <nehavaghela65@gmail.com>
2026-02-09 23:07:36 +0530

    first commit

---
+++ app/src/main/java/com/clg_complaint_Solver/quickresolve/SplashScreenActivity.java
@@ -0,0 +1,19 @@
+package com.clg_complaint_Solver.quickresolve;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+    }
+}
